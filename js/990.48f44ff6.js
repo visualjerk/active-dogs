1687c079e756ce2834bfb4fae61c85f8ecb1e773
visualjerk <visualjerk@users.noreply.github.com>
2022-02-18 21:57:34 +0000

    deploy: 8e501f9a1bcce3335e8b40fe8d3cfeaa62a33f65

--- js/990.48f44ff6.js
+++
@@ -1,6 +0,0 @@
-"use strict";(self["webpackChunkactive_dogs"]=self["webpackChunkactive_dogs"]||[]).push([[990],{8990:function(t,e,n){n.r(e),n.d(e,{createSwipeBackGesture:function(){return o}});var a=n(6587),r=n(6515);
-/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */
-const o=(t,e,n,o,s)=>{const c=t.ownerDocument.defaultView,i=t=>t.startX<=50&&e(),u=t=>{const e=t.deltaX,n=e/c.innerWidth;o(n)},d=t=>{const e=t.deltaX,n=c.innerWidth,r=e/n,o=t.velocityX,i=n/2,u=o>=0&&(o>.2||t.deltaX>i),d=u?1-r:r,l=d*n;let h=0;if(l>5){const t=l/Math.abs(o);h=Math.min(t,540)}s(u,r<=0?.01:(0,a.j)(0,r,.9999),h)};return(0,r.A)({el:t,gestureName:"goback-swipe",gesturePriority:40,threshold:10,canStart:i,onStart:n,onMove:u,onEnd:d})}}}]);
-//# sourceMappingURL=990.48f44ff6.js.map
