01312d4f55e0174acf4658f4e884eac3fe5cb536
visualjerk <jb@visualjerk.de>
2022-02-19 14:25:49 +0100

    add pwa

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
+const WorkboxPlugin = require('workbox-webpack-plugin')
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/active-dogs/' : '/',
+  publicPath: process.env.BASE_URL || '/',
+  configureWebpack: {
+    plugins: [
+      new WorkboxPlugin.GenerateSW({
+        // these options encourage the ServiceWorkers to get in there fast
+        // and not allow any straggling "old" SWs to hang around
+        clientsClaim: true,
+        skipWaiting: true,
+      }),
+    ],
+  },
 }
