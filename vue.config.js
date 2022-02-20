2dccc40b43654385fcf65eed4dec3aad48ef3027
visualjerk <jb@visualjerk.de>
2022-02-20 11:00:07 +0100

    fix base url in dev

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,15 @@
 const WorkboxPlugin = require('workbox-webpack-plugin')
-const ReplaceInFileWebpackPlugin = require('replace-in-file-webpack-plugin')
 
 const baseUrl = process.env.BASE_URL || '/'
+const BASE_URL_REGEX = new RegExp('<%= BASE_URL %>', 'g')
+
+function replaceBaseUrl(content) {
+  const stringContent = content.toString()
+  if (stringContent.match(BASE_URL_REGEX)) {
+    return stringContent.replace(BASE_URL_REGEX, baseUrl)
+  }
+  return content
+}
 
 module.exports = {
   publicPath: baseUrl,
@@ -13,18 +21,22 @@ module.exports = {
         clientsClaim: true,
         skipWaiting: true,
       }),
-      new ReplaceInFileWebpackPlugin([
+    ],
+  },
+  chainWebpack: (config) => {
+    config.plugin('copy').tap((args) => {
+      const newArgs = [
         {
-          dir: 'dist',
-          files: ['manifest.json'],
-          rules: [
+          patterns: [
             {
-              search: new RegExp('<%= BASE_URL %>', 'g'),
-              replace: baseUrl,
+              ...args[0].patterns[0],
+              transform: replaceBaseUrl,
             },
           ],
         },
-      ]),
-    ],
+      ]
+
+      return newArgs
+    })
   },
 }
