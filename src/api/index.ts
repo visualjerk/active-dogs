c83806c6bf7e1fa9ce73a19adbec979ef721e02b
visualjerk <jb@visualjerk.de>
2022-02-20 14:42:06 +0100

    add todos

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://mstepdozqbuywwiffixm.supabase.co'
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zdGVwZG96cWJ1eXd3aWZmaXhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQ3NzQxMTMsImV4cCI6MTk2MDM1MDExM30.hKjsQP_TjCVNDyrmzmA7pA0e8zbPIu7MDuqnawukllY'
+const supabaseKey = process.env.VUE_APP_SUPABASE_KEY
+if (!supabaseKey) {
+  throw 'Required environment variable "VUE_APP_SUPABASE_KEY" not found.'
+}
 export const supabase = createClient(supabaseUrl, supabaseKey)
